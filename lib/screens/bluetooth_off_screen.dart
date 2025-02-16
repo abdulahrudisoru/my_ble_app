35cd7eb6f149a23299313472f604ea77a24564b9
Cyrillus Rudi Soru <abdullahrudisoru@gmail.com>
2025-02-16 17:18:54 +0700

    Add Bluetooth Off Screen

---
+++ lib/screens/bluetooth_off_screen.dart
@@ -0,0 +1,12 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+class BluetoothOffScreen extends StatelessWidget {
+  const BluetoothOffScreen({super.key, required BluetoothAdapterState adapterState});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
